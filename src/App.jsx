e766d7bb4588e1879bc86aa00e5a896c7b04782d
Fede <fnzapa@gmail.com>
2024-03-25 18:17:28 +0100

    add new section

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <section> Modification </section>
+      <section> second modification </section>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
