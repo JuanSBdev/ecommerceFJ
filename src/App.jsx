cf6ee1d611b49922a547af3ffa415fd89ee7290a
Juan SB <juansb.dev@gmail.com>
2024-03-27 21:11:54 -0300

    React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import  useStore  from './zustand/store'
+import { Routes, Route  } from 'react-router-dom'
+
+import StartView  from './views/StartView'
 
 function App() {
-  let bears = useStore((state)=>state.bears)
-  let count = useStore((state)=>state.count)
+  // let bears = useStore((state)=>state.bears)
+  // let count = useStore((state)=>state.count)
 
-  //asi traemos en una sola renderización de a mas estados 
-  const { incrementCount, increasePopulation } = useStore(state => ({
-    incrementCount: state.increaseCount,
-    increasePopulation: state.increasePopulation
-  }))
+  // //asi traemos en una sola renderización de a mas estados 
+  // const { incrementCount, increasePopulation } = useStore(state => ({
+  //   incrementCount: state.increaseCount,
+  //   increasePopulation: state.increasePopulation
+  // }))
 
   return (
     <>
-
-      <section> Modification </section>
-      <section> second modification </section>
-      <section> 3° modification </section>
-
-      <div>
-        
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <button onClick={increasePopulation}> + bears </button>
-        <button onClick={incrementCount}> + count </button>
-        <p>
-        the Bears are {bears} 
-
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <p>asd</p>
+    <Routes>
+       <Route path="/asd" element={  <StartView/>  }  />
+    </Routes>
     </>
   )
 }
--- src/views/StartView.jsx
+++ src/views/StartView.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 
+import useStore from '../zustand/store'
+
 export default function StartView() {
+    let {count, increasePopulation, incrementCount, bears }  = useStore((state)=> ({
+        count: state.count, 
+        bears: state.bears,
+        increasePopulation: state.increasePopulation,
+        incrementCount: state.increaseCount
+
+
+    }))    
   return (
-    <div>StartView</div>
+    <div>
+
+    
+      <h1>Ecommerce con Fede</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+        <button onClick={increasePopulation}> + bears </button>
+        <button onClick={incrementCount}> + count </button>
+        <p>
+        the Bears are {bears} 
+
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
   )
 }
