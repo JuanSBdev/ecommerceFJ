44030143cf3d5730c9bbf62af63359f340f82b68
Juan SB <juansb.dev@gmail.com>
2024-05-26 23:02:36 -0300

    fixing card sizes

--- src/views/StartView.jsx
+++ src/views/StartView.jsx
@@ -19,29 +19,11 @@ export default function StartView() {
 
 
         <h1>Products</h1>
-        <div className="flex">
+        <div className="">
           <Card/>
 
         </div>
     
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <button onClick={increasePopulation}> + bears </button>
-        <button onClick={incrementCount}> + count </button>
-        <p>
-        the Bears are {bears} 
-
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-
 
     </div>
   )
